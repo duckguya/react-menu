32a299eccca19641978af8ea30446c437d61acdf
duckguya <duckguya@gmail.com>
2023-04-14 01:45:11 +0900

    test

--- src/Components/GridItems.js
+++ src/Components/GridItems.js
@@ -7,7 +7,7 @@ function GridItems() {
 const Container = styled.div`
   height: calc(100vh - 100px);
   /* height: 90vh; */
-  margin: 0px 50px 50px 350px;
+  margin: 50px 50px 50px 350px;
   box-shadow: gray 1px 1px 15px -5px;
   border-radius: 15px;
 `;
